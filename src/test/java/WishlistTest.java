95e62baea8aa233c0c74336067b3cc0ffd596327
RAMONA SAUCINITANU <saucinitanu.ramona@yahoo.com>
2021-12-30 11:57:49 +0200

    Task

--- src/test/java/WishlistTest.java
+++ src/test/java/WishlistTest.java
@@ -1,3 +1,5 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -5,20 +7,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WishlistTest {
 
+    private WebDriver driver;
+
+    @Before
+    public void openBrowser() {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://testfasttrackit.info/selenium-test/");
+    }
+
     @Test
 
-    public void addToWishlistTest (){
-        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://testfasttrackit.info/selenium-test/");
+    public void addToWishlistTest () {
         driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-5.parent > a")).click();
         driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col3-layout > div > div.col-wrapper > div.col-main > div.category-products > ul > li > div > div.actions > a")).click();
         driver.findElement(By.cssSelector("#product_addtocart_form > div.product-shop > div.product-options-bottom > ul.add-to-links > li:nth-child(1) > a")).click();
+    }
+
+
+    @After
+    public void quit () {
         driver.quit();
-
-
-
-
     }
 
 }
